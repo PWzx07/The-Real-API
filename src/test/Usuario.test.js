b0d3e2af979631697dab20a66a49766cc874a049
Pedro W. <Pazevedodejesus@gmail.com>
2025-05-26 19:40:20 -0300

    :test_tube:test: Testes de rotas atualizado

--- src/test/Usuario.test.js
+++ src/test/Usuario.test.js
@@ -7,4 +7,22 @@ describe("Testes da rota /usuario", () => {
     const res = await request(app).get("/usuario/");
     expect(res.statusCode).toBe(200);
   });
-});  
+
+  test("GET /usuario/:id", async ()=>{
+    const id = "c7bd2e66-4d4c-4453-a3ed-045894601af1";
+    const res = await request(app).get(`/usuario/${id}`);
+    expect(res.statusCode).toBe(200); 
+  });
+
+  test("POST /usuario/registra/", async ()=>{
+    const res = await request(app).post("/usuario/registra/").send({
+      nome: "Teste Jest",
+      email: `jest${Date.now()}@mail.com`, // único a cada teste
+      senha: "123456",
+      tipo: "CLIENTE"
+    });
+
+    expect(res.statusCode).toBe(201);
+    expect(res.body).toHaveProperty("newUsuario");
+  });
+});
